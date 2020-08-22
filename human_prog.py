cbc7055328ce90d1cdf423ccb71edaff3f36d43c
MayankChaturvedi14 <69956839+MayankChaturvedi14@users.noreply.github.com>
2020-08-22 22:01:30 +0530

    Add files via upload

---
+++ human_prog.py
@@ -0,0 +1,39 @@
+import pyttsx3
+import os
+pyttsx3.speak("Welcome to my Softwares")
+while True:
+    print("------------------------------------------------")
+    print("Open any softwares as your wish: ",end='')
+    p = input()
+    if((("run" in p) or ("execute" in p) or ("open" in p)) and (("chrome browser" in p) or ("browser" in p))):
+        pyttsx3.speak("Opening the chrome browser")
+        os.system("chrome")
+
+    elif((("run" in p) or ("execute" in p) or ("open" in p) or ("play" in p)) and (" media player" in p)):
+        pyttsx3.speak("Opening the windows media player")
+        os.system("wmplayer")
+
+    elif((("run" in p) or ("execute" in p) or ("open" in p) or ("play" in p)) and ("video player" in p)):
+        pyttsx3.speak("Opening the vlc player")
+        os.system("vlc")
+
+    elif((("run" in p) or ("execute" in p) or ("open" in p)) and (("text" in p) or ("notepad" in p) or ("editor" in p))):
+        pyttsx3.speak("Opening the notepad IDE")
+        os.system("notepad")
+
+    elif((("run" in p) or ("execute" in p) or ("open" in p)) and (("text" in p) or ("codeblocks" in p) or ("editor" in p))):
+        pyttsx3.speak("Opening the CodeBlocks IDE")
+        os.system("codeblocks")
+
+    elif((("run" in p) or ("execute" in p) or ("open" in p)) and (("text" in p) or ("DEV C++" in p) or ("editor" in p) or ("IDE" in p))):
+        pyttsx3.speak("Opening the Dev C++ IDE")
+        os.system("devcpp")
+
+    elif("exit" in p) or ("quit" in p):
+        break
+
+    else:
+        pyttsx.speak("Programs are not avalilable")
+        print("---------------------------------------")
+        print("Does not support")
+        print("---------------------------------------")
